3ccd2195d2f99800ef681a723eb92b2ea4fc0d3f
afffehh <enestromalfred@gmail.com>
2024-11-27 14:37:30 +0100

    cscscscs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-System.Console.WriteLine("dsdsds");
+﻿System.Console.WriteLine(dsdhfdsfsdfsdfsfsf);
 
